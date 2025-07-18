06a65d5deb2d58c8b4e607047e01825dff881ec1
Ugonna Umunna <uumunna@montgomerycollege.edu>
2025-07-18 00:06:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-document.getElementById("clickBtn").addEventListener("click", function () {
-    alert("You clicked the button!");
-  });
-  
+function viewDetails(propertyName) {
+  alert(`Showing details for: ${propertyName}`);
+}
